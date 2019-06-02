bda50d5b86cee17048a10fd94df79497e3a78f24
Anja <rasoloanja@gmail.com>
2019-06-02 00:12:07 -0400

    fixed animation

--- animation.js
+++ animation.js
@@ -1,17 +1,10 @@
 $(document).ready(function(){ 
 
 
-    var tl = new TimelineMax({repeat:-1});
-    
-    tl
-    .to(".intro__layout--1", 1.5, {autoAlpha: 0}, '+=10')
-    .to(".intro__layout--1", 2, {autoAlpha: 1}, '+=10')
 
 
-    var t2 = new TimelineMax({repeat:-1});
-    
-    t2
-
+    var t1 = new TimelineMax({repeat:-1});  
+    t1
     .to(".intro--1", 1.5, {autoAlpha: 0}, '+=10')
     .to(".intro--1", 2, {autoAlpha: 1}, '+=10')
 })
