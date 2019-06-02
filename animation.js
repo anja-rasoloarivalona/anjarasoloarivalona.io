1012bf0a327242e6a92da90185306030df312653
Anja <rasoloanja@gmail.com>
2019-06-02 16:29:34 -0400

    Added img to projects

--- animation.js
+++ animation.js
@@ -5,6 +5,6 @@ $(document).ready(function(){
 
     var t1 = new TimelineMax({repeat:-1});  
     t1
-    .to(".intro--1", 1.5, {autoAlpha: 0}, '+=10')
-    .to(".intro--1", 2, {autoAlpha: 1}, '+=10')
+    .to(".intro--1", 1.5, {autoAlpha: 0}, '+=5')
+    .to(".intro--1", 2, {autoAlpha: 1}, '+=5')
 })
